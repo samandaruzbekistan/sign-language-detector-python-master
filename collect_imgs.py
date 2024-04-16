eb54da43c8c04de0ffa200c495ed276b73a3741d
Samandar Sariboyev <samandarsariboyev69@gmail.com>
2024-04-16 09:02:28 +0500

    Initial commit

---
+++ collect_imgs.py
@@ -0,0 +1,54 @@
+import os
+import cv2
+
+DATA_DIR = './data'
+if not os.path.exists(DATA_DIR):
+    os.makedirs(DATA_DIR)
+
+number_of_classes = 5
+dataset_size = 100
+
+# Try different camera indices until one works
+for camera_index in range(4):  # Assuming maximum of 4 cameras
+    cap = cv2.VideoCapture(camera_index)
+    if cap.isOpened():
+        print(f"Using camera index {camera_index}")
+        break
+else:
+    print("Error: No camera found.")
+    exit()
+
+for j in range(number_of_classes):
+    class_dir = os.path.join(DATA_DIR, str(j))
+    if not os.path.exists(class_dir):
+        os.makedirs(class_dir)
+
+    print('Collecting data for class {}'.format(j))
+
+    done = False
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        cv2.putText(frame, 'Ready? Press "Q" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+                    cv2.LINE_AA)
+        cv2.imshow('frame', frame)
+        if cv2.waitKey(25) == ord('q'):
+            break
+
+    counter = 0
+    while counter < dataset_size:
+        ret, frame = cap.read()
+        if not ret:
+            print("Error: Failed to capture frame.")
+            break
+
+        cv2.imshow('frame', frame)
+        cv2.waitKey(25)
+        cv2.imwrite(os.path.join(class_dir, '{}.jpg'.format(counter)), frame)
+        counter += 1
+
+cap.release()
+cv2.destroyAllWindows()
